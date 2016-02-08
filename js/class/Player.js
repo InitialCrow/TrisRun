47fa34acbd962d945fa3303080c060cd75bbf1c2
Adam Parent <Adam Parent>
2016-02-08 08:07:42 +0100

    Trisrun alpha -- 0.0.7 // fixes upgrade game engine add content

--- js/class/Player.js
+++
@@ -1,8 +0,0 @@
-function Map(name, size, skybox){
-	this.name = name || "No-name-map" ;
-	this.size = size || 1900;
-	this.skybox = skybox || "insert assets";
-}
-Map.prototype.init = function() {
-	console.log("--------- classe Map --------");
-};
